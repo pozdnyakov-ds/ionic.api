42462c42002d19373a30bf2445737e17af25a604
Dmitry Pozdnyakov <2903015@gmail.com>
2023-11-22 12:35:56 +0300

    v1.9

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const v1DeviceRouter = require('./v1/routes/deviceRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({
-    extended: false
+app.use(express.urlencoded({
+    extended: true
 }));
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/v1/device', v1DeviceRouter);
 
